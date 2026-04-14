a2bbcc410333370260f50726fb32d629237c8989
SS-mr <aiai.masa.0@ezweb.ne.jp>
2026-04-14 23:41:42 +0900

    DIなしのPGを実装

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// メッセージを送るクラス
+public class MessageService
+{
+  public void Send(string message)
+  {
+    Console.WriteLine($"送信内容: {message}");
+  }
+}
+
+// 通知を行うクラス
+public class Notification
+{
+  private readonly MessageService _messageService = new MessageService();
+
+  public void Notify()
+  {
+    _messageService.Send("DIなしの通知です");
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    // Notification作成
+    var notification = new Notification();
+
+    // 通知を実行する
+    notification.Notify();
+  }
+}
