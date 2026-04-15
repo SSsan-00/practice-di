a67b8d699c73d2dc949a58e82bc422744fe912c7
SS-mr <aiai.masa.0@ezweb.ne.jp>
2026-04-15 21:11:47 +0900

    テストコードを記述

--- practice-di.Tests/Test1.cs
+++ practice-di.Tests/Test1.cs
@@ -1,10 +1,44 @@
-﻿namespace practice_di.Tests;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// テスト用プロジェクトの名前空間
+namespace practice_di.Tests;
+
+// IMessageService のテスト実装
+// 呼び出されたかどうかを確認するために、値を保持する
+public class TestMessageService : IMessageService
+{
+    // Send が呼ばれたかどうか
+    public bool WasCalled { get; private set; }
+
+    // 渡されたメッセージを保持する
+    public string? SentMessage { get; private set; }
+    
+    public void Send(string message)
+    {
+        WasCalled = true;
+        SentMessage = message;
+    }
+}
 
 [TestClass]
-public sealed class Test1
+public class NotificationTests
 {
     [TestMethod]
-    public void TestMethod1()
+    public void NotifyAfterCalledSend()
     {
+        // Arrange
+        // テスト用の IMessageService 実装を作る
+        var testMessageService = new TestMessageService();
+
+        // Notifycation に外から渡す
+        var notifycation = new Notification(testMessageService);
+
+        // Act
+        // 実際に通知処理を実行する
+        notifycation.Notify();
+
+        // Assert
+        // Send が呼ばれたことを確認する
+        Assert.IsTrue(testMessageService.WasCalled);
     }
 }
